e659ab274e86e789ff8e966bf37e469043bba181
Alex Kharouk <akharoukpp@gmail.com>
2021-11-24 17:16:47 +0000

    Adds a bit of styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,17 @@
+import type { LinksFunction } from "remix"
+import styleSheet from "../styles/index.css"
+
+console.log({ styleSheet })
+
+export let links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: styleSheet
+    }
+  ]
+}
+
 export default function IndexPage() {
   return (
     <div>
