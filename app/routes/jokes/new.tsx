5c595c780a4d8921df322c56a0b6f96b1ebbf2ca
Alex Kharouk <akharoukpp@gmail.com>
2021-11-24 07:08:21 +0000

    Adds form

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -2,9 +2,17 @@ export default function NewJokeOutlet() {
   return (
     <div>
       <form>
+        <label htmlFor="name">
+          Name:
+          <input name="name" type="text" />
+        </label>
+        <br />
         <label htmlFor="content">
+          Content:
           <input name="content" type="text" />
         </label>
+        <br />
+        <button type="submit">Add</button>
       </form>
     </div>
   )
